8ebb17805a99e463f32289fcf2c63bdd79f74a80
Otavio Lopes <60718044+OtavioLopes5@users.noreply.github.com>
2022-07-07 12:37:58 -0300

    Update GeradorCS.py

--- GeradorCS.py
+++ GeradorCS.py
@@ -1,1 +1,1 @@
-from random import SystemRandom as sr ; import string as s; print(''.join(sr().choices(s.ascii_punctuation + s.punctuation,  k = 12 )))
+from random import SystemRandom as sr ; import string as s; print(''.join(sr().choices(s.ascii_letters + s.punctuation,  k = 12 )))
